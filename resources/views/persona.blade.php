9afc0851dc1efd2097c327800d4e2d69bdc3bd09
danielDelgado123 <delgadodaniel413@gmail.com>
2020-08-23 16:42:31 -0400

    Se Restauro a su oficial

--- resources/views/persona.blade.php
+++
@@ -1,1 +0,0 @@
-<p class="h1">Formulario de la Persona: <b>{{$nombre}}</b> edad: <b>{{$edad}}</b></p>
--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/form-persona/{nombre?}/{edad?}', function ($nombre = "Amilcar",$edad = "12") {
-    // return view('persona', array(
-    //     "nombre" => $nombre,
-    //     "edad" => $edad
-    // ));
-    return view('persona')
-        ->with('nombre', $nombre)
-        ->with('edad', $edad);
-})
-->where([
-    'edad' => '[0-9]+',
-    'nombre' => '[a-zA-Z ]+'
-]);
